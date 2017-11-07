2b4aeff5a144971b73f1148a51cd2de3e18a3568
Logan Cooper <ldtcoop@gmail.com>
2017-11-06 17:13:50 -0800

    Move prime factorization function

--- p5/p5.rb
+++ p5/p5.rb
@@ -1,3 +1,9 @@
+require_relative 'prime_factorization.rb'
+
 def p5
-  
+  big_prod = (2..20).reduce(:*)
+  big_prod_factors = prime_factorization(big_prod)
+  big_prod_factors.reduce(:*)
 end
+
+p p5
