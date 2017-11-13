d471f4daeb3d7dbe79f20a6c39630365005b82ec
Logan Cooper <ldtcoop@gmail.com>
2017-11-12 16:51:46 -0800

    Remove comments

--- p6/p6.js
+++ p6/p6.js
@@ -8,22 +8,4 @@ function problemSix(n) {
   return( (sum * sum) - sumOfSquares);
 }
 
-// function problemSix() {
-//   let squareSums = squareOfSums(100);
-//   let sumSquares = sumOfSquares(100);
-//   return sumSquares - squareSums;
-// }
-//
-// function squareOfSums(n) {
-//   let sum = (1 * (n + 1)) / 2;
-//   return sum * sum;
-// }
-//
-// function sumOfSquares(n) {
-//   let a = (n * n * n) / 3;
-//   let b = (n * n) / 2;
-//   let c = n / 6;
-//   return a + b + c;
-// }
-
 console.log(problemSix(100));
